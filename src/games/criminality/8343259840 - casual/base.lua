f368adf8f9f7196edac99b4873da933683f7c9ab
stepa444dss-wq <stepa444dss-wq@users.noreply.github.com>
2026-08-31 15:57:17 +0700

    Update Criminality: remove Killaura, TargetStrafe, Jesus, Timer, Swim and add AntiFall stub

--- src/games/criminality/8343259840 - casual/base.lua
+++ src/games/criminality/8343259840 - casual/base.lua
@@ -7,6 +7,18 @@ end
 
 local vape = shared.vape
 
-for _, v in {'Invisible', 'HitBoxes'} do
+for _, v in {'Invisible', 'HitBoxes', 'Killaura', 'TargetStrafe', 'Jesus', 'Timer', 'Swim'} do
 	vape:Remove(v)
 end
+
+-- AntiFall stub for Criminality
+vape:Remove('AntiFall')
+vape.Categories.Blatant:CreateModule({
+	Name = 'AntiFall',
+	Function = function(callback)
+		if callback then
+			vape:CreateNotification('AntiFall', 'AntiFall is currently a stub for Criminality', 3, 'info')
+		end
+	end,
+	Tooltip = 'Prevents you from falling into the void (WIP).'
+})
